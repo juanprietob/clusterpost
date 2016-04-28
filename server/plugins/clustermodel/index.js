32dd5cdfdbda1daa33f6802c60340e61cb3309cb
Juan Carlos Prieto <juanprietob@gmail.com>
2016-04-27 21:23:10 -0400

    ENH: The clustermodel plugin describes the Joi implementation of different objects from the clusterpost application
    The JSON objects of the application are described by their Joi implementation and use for validation during runtime.

---
+++ server/plugins/clustermodel/index.js
@@ -0,0 +1,75 @@
+var Joi = require('Joi');
+
+var clustermodel = {};
+
+clustermodel.parameter = Joi.object().keys({
+	flag: Joi.string().allow(''),
+  	name: Joi.string().allow('')
+});
+
+clustermodel.output = Joi.object().keys({
+	type: Joi.string().valid('file', 'directory', 'tar.gz'), 
+  	name: Joi.string()
+});
+
+clustermodel.input = Joi.object().keys({
+  	name: Joi.string(),
+  	remote : Joi.object().keys({
+  		serverCodename: Joi.string().optional(),
+  		uri: Joi.string()
+  	}).optional()
+});
+
+clustermodel.jobpost = Joi.object().keys({
+		type: Joi.string().required(),
+		userEmail: Joi.string().required(),			
+		executionserver: Joi.string().required(),
+		jobparameters: Joi.array().items(clustermodel.parameter).optional(),
+		executable: Joi.string().required(),			
+		parameters: Joi.array().items(clustermodel.parameter).min(1),
+		inputs: Joi.array().items(clustermodel.input).min(1),
+		outputs: Joi.array().items(clustermodel.output).min(1)
+    });
+
+clustermodel.jobstatus = Joi.object().keys({
+		status: Joi.string().valid('CREATE', 'DOWNLOADING', 'RUN', 'FAIL', 'KILL', 'UPLOADING', 'EXIT', 'DONE'),
+		jobid: Joi.number().optional(),
+		stat: Joi.optional(),
+		error: Joi.optional(),
+		downloadstatus: Joi.array().items(Joi.object()).optional(),
+		uploadstatus: Joi.array().items(Joi.object()).optional()
+	});
+
+clustermodel.job = Joi.object().keys({
+		_id: Joi.string().alphanum().required(),
+		_rev: Joi.string().required(),
+		type: Joi.string().required(),
+		userEmail: Joi.string().email().required(),
+		timestamp: Joi.date().required(),
+		jobstatus: clustermodel.jobstatus.required(),
+		executable: Joi.string().required(),
+		executionserver: Joi.string().required(),
+		jobparameters: Joi.optional(),
+		parameters: Joi.array().items(clustermodel.parameter).min(1),			
+		inputs: Joi.array().items(clustermodel.input).min(1),
+		outputs: Joi.array().items(clustermodel.output).min(1),
+		_attachments: Joi.optional()
+    });
+
+
+clustermodel.user = Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().regex(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])[\w\d!@#$%_-]{6,40}$/)
+});
+
+clustermodel.login = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().regex(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])[\w\d!@#$%_-]{6,40}$/)
+});
+
+
+exports.job = clustermodel.job;
+exports.jobpost = clustermodel.jobpost;
+exports.user = clustermodel.user;
+exports.login = clustermodel.login;
