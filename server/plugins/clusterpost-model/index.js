08d73dbb888678c3d96108d5911f4691fb9a977c
Juan Carlos Prieto <juanprietob@gmail.com>
2016-04-28 02:48:21 -0400

    STYLE: Use export variable to export the Joi objects

--- server/plugins/clusterpost-model/index.js
+++ server/plugins/clusterpost-model/index.js
@@ -1,18 +1,16 @@
 var Joi = require('Joi');
 
-var clustermodel = {};
-
-clustermodel.parameter = Joi.object().keys({
+exports.parameter = Joi.object().keys({
 	flag: Joi.string().allow(''),
   	name: Joi.string().allow('')
 });
 
-clustermodel.output = Joi.object().keys({
+exports.output = Joi.object().keys({
 	type: Joi.string().valid('file', 'directory', 'tar.gz'), 
   	name: Joi.string()
 });
 
-clustermodel.input = Joi.object().keys({
+exports.input = Joi.object().keys({
   	name: Joi.string(),
   	remote : Joi.object().keys({
   		serverCodename: Joi.string().optional(),
@@ -20,18 +18,18 @@ clustermodel.input = Joi.object().keys({
   	}).optional()
 });
 
-clustermodel.jobpost = Joi.object().keys({
+exports.jobpost = Joi.object().keys({
 		type: Joi.string().required(),
 		userEmail: Joi.string().required(),			
 		executionserver: Joi.string().required(),
-		jobparameters: Joi.array().items(clustermodel.parameter).optional(),
+		jobparameters: Joi.array().items(exports.parameter).optional(),
 		executable: Joi.string().required(),			
-		parameters: Joi.array().items(clustermodel.parameter).min(1),
-		inputs: Joi.array().items(clustermodel.input).min(1),
-		outputs: Joi.array().items(clustermodel.output).min(1)
+		parameters: Joi.array().items(exports.parameter).min(1),
+		inputs: Joi.array().items(exports.input).min(1),
+		outputs: Joi.array().items(exports.output).min(1)
     });
 
-clustermodel.jobstatus = Joi.object().keys({
+exports.jobstatus = Joi.object().keys({
 		status: Joi.string().valid('CREATE', 'DOWNLOADING', 'RUN', 'FAIL', 'KILL', 'UPLOADING', 'EXIT', 'DONE'),
 		jobid: Joi.number().optional(),
 		stat: Joi.optional(),
@@ -40,36 +38,30 @@ clustermodel.jobstatus = Joi.object().keys({
 		uploadstatus: Joi.array().items(Joi.object()).optional()
 	});
 
-clustermodel.job = Joi.object().keys({
+exports.job = Joi.object().keys({
 		_id: Joi.string().alphanum().required(),
 		_rev: Joi.string().required(),
 		type: Joi.string().required(),
 		userEmail: Joi.string().email().required(),
 		timestamp: Joi.date().required(),
-		jobstatus: clustermodel.jobstatus.required(),
+		jobstatus: exports.jobstatus.required(),
 		executable: Joi.string().required(),
 		executionserver: Joi.string().required(),
 		jobparameters: Joi.optional(),
-		parameters: Joi.array().items(clustermodel.parameter).min(1),			
-		inputs: Joi.array().items(clustermodel.input).min(1),
-		outputs: Joi.array().items(clustermodel.output).min(1),
+		parameters: Joi.array().items(exports.parameter).min(1),			
+		inputs: Joi.array().items(exports.input).min(1),
+		outputs: Joi.array().items(exports.output).min(1),
 		_attachments: Joi.optional()
     });
 
 
-clustermodel.user = Joi.object().keys({
+exports.user = Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().regex(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])[\w\d!@#$%_-]{6,40}$/)
 });
 
-clustermodel.login = Joi.object().keys({
+exports.login = Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().regex(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])[\w\d!@#$%_-]{6,40}$/)
 });
-
-
-exports.job = clustermodel.job;
-exports.jobpost = clustermodel.jobpost;
-exports.user = clustermodel.user;
-exports.login = clustermodel.login;
