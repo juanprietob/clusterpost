29b9013622af46d6d881c9e9bccc3593068148b7
Juan Carlos Prieto <juanprietob@gmail.com>
2016-06-03 13:34:11 -0400

    BUG: Set status as FAIL when upload is unsuccessful
    Remove the option from job submit and force the user to use the 'force' option to resubmit the job.

--- src/clusterpost-execution/jobstatus.js
+++ src/clusterpost-execution/jobstatus.js
@@ -19,6 +19,8 @@ module.exports = function(doc, conf){
             }
             if(alluploadstatus){
                 docupdated.jobstatus.status = "DONE";
+            }else{
+                docupdated.jobstatus.status = "FAIL";
             }
             return executionmethods.uploadDocumentDataProvider(docupdated);
         });
