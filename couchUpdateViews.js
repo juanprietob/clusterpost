e200166f90b5ae8fc392a992a02b6c138c6a8226
Juan Carlos Prieto <juanprietob@gmail.com>
2016-06-06 16:24:24 -0400

    BUG: Main cript to perform migration
    Call the appropriate function from the module to produce the migration

--- couchUpdateViews.js
+++ couchUpdateViews.js
@@ -1,2 +1,14 @@
-var cuv = require('couch-update-views');
-cuv.couchUpdateViews();
+var clusterpostserver = require('clusterpost-server');
+
+
+clusterpostserver.migrateUp()
+.then(function(res){
+    console.log(res);
+    process.exit(0);
+})
+.catch(function(err){
+    console.log(error);
+    process.exit(1);
+});
+
+
