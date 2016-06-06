76077dbb990a5cbbdde6e49063c6ccee5bf88734
Juan Carlos Prieto <juanprietob@gmail.com>
2016-06-06 14:40:18 -0400

    ENH: Create package couch-update-views
    Update and synchronize your remote couchdb views with the content of a local folder

--- src/couch-update-views/migrateUP.js
+++ src/couch-update-views/migrateUP.js
@@ -1,113 +1,99 @@
-var fs = require('fs');
-var child_process = require('child_process');
+
+var _ = require('underscore');
 var request = require('request');
+var fs = require('fs');
 var Promise = require('bluebird');
-var qs = require('querystring');
 var path = require('path');
 
-var env = process.env.NODE_ENV;
+module.exports = function(dburl, viewsdir){
 
-if(!env){
-	console.error("Please set NODE_ENV variable.");
-	return 1;	
+    const createDB = function(url){
+        return new Promise(function(resolve, reject){
+            request.put(url, function(err, res, body){
+                if(err){
+                    reject(err.message);
+                }else{
+                    try{
+                        if(JSON.parse(body).error === "not_found"){
+                            request.put(url, function(err, res, body){
+                                resolve(JSON.parse(body));
+                            });
+                        }else{
+                            resolve(JSON.parse(body));
+                        }
+                    }catch(e){
+                        console.error(url);
+                        console.error(e);
+                        reject(e);
+                    }
+                }
+            });
+        });
+    }
+
+    const updateDocuments = function(url, viewsdir){
+
+        var fileviews = fs.readdirSync(viewsdir); 
+
+        var testView = function(file){
+            return new Promise(function(resolve, reject){
+                try{
+                    if(file.indexOf(".json") === -1) reject(file);
+
+                    fs.readFile(path.join(viewsdir, file), function (err, data) {
+                        if (err) throw err;
+                        
+                        var designdoc = JSON.parse(data);
+
+                        var options = {
+                            uri: url + "/" + designdoc._id
+                        }
+
+                        request(options, function(err, res, body){
+                            var couchdesigndoc = JSON.parse(body);
+
+                            if(JSON.stringify(designdoc.views) !== JSON.stringify(couchdesigndoc.views)){
+
+                                var uri = url + "/" + designdoc._id;
+                                if(couchdesigndoc._rev){
+                                    designdoc._rev = couchdesigndoc._rev;
+                                    uri += "?rev="+designdoc._rev;
+                                }
+                                console.log(couchdesigndoc);
+                                var options = {
+                                    uri :  uri,
+                                    method : 'PUT',
+                                    json : designdoc
+                                }
+
+                                request(options, function(err, res, body){
+                                    if(err){
+                                        reject(err.message);
+                                    }else{
+                                        resolve(body);
+                                    }
+                                });
+
+                            }else{                                
+                                resolve({
+                                	error: "No changes in document.",
+                                	couchDB: url, 
+                                	view: viewsdir+file
+                                });
+                            }
+                        });
+                    });
+                }catch(e){
+                    reject(e);
+                }
+            });
+        }
+
+        return Promise.map(fileviews, testView);
+    }
+
+    return createDB(dburl)
+    .then(function(result){
+        return updateDocuments(dburl, viewsdir);
+    });
 }
-
-const getConfigFile = function (env, base_directory) {
-  try {
-    // Try to load the user's personal configuration file
-    return require(base_directory + '/conf.my.' + env + '.json');
-  } catch (e) {
-    // Else, read the default configuration file
-    return require(base_directory + '/conf.' + env + '.json');
-  }
-};
-
-const createDB = function(name){
-	return new Promise(function(resolve, reject){
-		request.put(name, function(err, res, body){
-			if(err) reject(err.message);
-			try{
-				if(JSON.parse(body).error === "not_found"){
-					request.put(name, function(err, res, body){
-						resolve(JSON.parse(body));
-					});
-				}else{
-					resolve(JSON.parse(body));
-				}
-			}catch(e){
-				console.error(name);
-				console.error(e);
-				reject(e);
-			}
-			
-		})
-	});
-}
-
-const updateDocuments = function(db, viewsdir){
-
-	var fileviews = fs.readdirSync(viewsdir); 
-
-	var testView = function(file){
-		return new Promise(function(resolve, reject){
-			try{
-				if(file.indexOf(".json") === -1) reject(file);
-
-				fs.readFile(viewsdir+file, function (err, data) {
-					if (err) throw err;
-					
-					var designdoc = JSON.parse(data);
-
-					var options = {
-						uri: db + "/" + designdoc._id
-					}
-
-					request(options, function(err, res, body){
-						var couchdesigndoc = JSON.parse(body);
-
-				  		if(JSON.stringify(designdoc.views) !== JSON.stringify(couchdesigndoc.views)){
-
-				  			var uri = db + "/" + designdoc._id;
-				  			if(couchdesigndoc._rev){
-				  				designdoc._rev = couchdesigndoc._rev;
-				  				uri += "?rev="+designdoc._rev;
-				  			}
-					  		console.log(couchdesigndoc);
-					  		var options = {
-								uri :  uri,
-								method : 'PUT',
-								json : designdoc
-							}
-
-							request(options, function(err, res, body){
-								if(err) reject(err.message);
-								resolve(body);
-							});
-
-					  	}else{
-				  			console.log("No changes in document: " + viewsdir+file + ", in db: " + db);
-				  		}
-					});
-				});
-			}catch(e){
-				reject(e);
-			}
-		});
-	}
-
-	return Promise.map(fileviews, testView);
-}
-
-var basedir = path.dirname(process.argv[1]);
-var conf = getConfigFile(env, "../");
-var dataprovider = conf.dataproviders[conf.default.dataprovider];
-var clusterdb = dataprovider.hostname + "/" + dataprovider.database;
-var viewsdir = basedir + '/views/';
-
-createDB(clusterdb)
-.then(function(result){
-	return updateDocuments(clusterdb, viewsdir);
-})
-.then(console.log)
-.catch(console.error)
-
--- src/couch-update-views/updateDesignDocument.js
+++ src/couch-update-views/updateDesignDocument.js
@@ -1,68 +1,33 @@
 
-var help = function(){
-    console.error("help: Update a document in the views folder with the version running at some couchdb instance. Default: localhost:5984");
-    console.error("NODE_ENV=production|test " + process.argv[0] + " " + process.argv[1] + " -design <name of design document>");
-    return 1;
-}
-
-const getConfigFile = function (env, base_directory) {
-  try {
-    // Try to load the user's personal configuration file
-    return require(base_directory + '/conf.my.' + env + '.json');
-  } catch (e) {
-    // Else, read the default configuration file
-    return require(base_directory + '/conf.' + env + '.json');
-  }
-};
-
-var design = undefined;
-
-for(var i = 0; i < process.argv.length; i++){
-    if(i < process.argv.length - 1){
-        if(process.argv[i] === "-design"){
-            design = process.argv[i+1];
-        }
-    }
-}
-
-var env = process.env.NODE_ENV;
-
-if(design === undefined || !env){
-    if(!env){
-        console.error("Please set NODE_ENV variable.");
-    }
-    return help();
-}
-
+var _ = require('underscore');
 var Promise = require('bluebird');
 var request = require('request');
 var fs = require('fs');
 var path = require('path');
 
+module.exports = function(couchdb, viewsDir, design){
+    
+    var uri = couchdb + "/_design/" + design;
 
-var basedir = path.dirname(process.argv[1]);
-var conf = getConfigFile(env, basedir);
-var dataprovider = conf.dataproviders[conf.default.dataprovider];
-var uri = dataprovider.hostname + "/" + dataprovider.database + "/_design/" + design;
-
-request(uri, function(err, res, body){
-	if(err) {
-		console.error(err);
-		return 1;
-	}
-	if(body !== ""){
-		var jsonbody = JSON.parse(body);
-		delete jsonbody._rev;
-		var jsonstring = JSON.stringify(jsonbody, null, 4);
-        var viewfilename = basedir + '/views/' + design + '.json';
-		var write = fs.writeFile(viewfilename, jsonstring, function(err){
-            if(err) {
-                console.error(err)
-                return 1;
+    return new Promise(function(resolve, reject){
+        request(uri, function(err, res, body){
+            if(err) {                
+                reject(err);
+            }else{
+                var jsonbody = JSON.parse(body);
+                delete jsonbody._rev;
+                var jsonstring = JSON.stringify(jsonbody, null, 4);                
+                var viewfilename = path.join(viewsDir, design + '.json');
+                try{
+                    var write = fs.writeFileSync(viewfilename, jsonstring);
+                    resolve({
+                        ok: true,
+                        id: viewfilename
+                    });
+                }catch(err){
+                    reject(err);
+                }
             }
-
-            console.log('View updated: ', viewfilename);
         });
-	}
-	
-});
+    });
+}
