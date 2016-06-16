28e8575eba9aba5b21750187c836b447cd6bec2b
juanprietob <juanprietob@gmail.com>
2016-06-15 22:00:29 -0500

    PERF: Use the package hapi-jwt-couch for user aunthentication
    Add function to support the execution servers validation

--- src/clusterpost-auth/index.js
+++ src/clusterpost-auth/index.js
@@ -1,8 +1,22 @@
-exports.register = function (server, conf, next) {
-  
-  require('./jwtauth.routes')(server, conf);
+var Boom = require('boom');
 
-  return next();
+exports.register = function (server, conf, next) {
+
+	const validate = function(req, decodedToken, callback){
+        var exs = server.methods.executionserver.getExecutionServer(decodedToken.executionserver);
+		if(exs){
+			exs.scope = ['executionserver'];
+			callback(undefined, true, exs);
+		}else{
+			callback(Boom.unauthorized(exs));
+		}
+	}
+
+	conf.validate = validate;
+
+	require('hapi-jwt-couch')(server, conf);
+
+	return next();
 };
 
 exports.register.attributes = {
