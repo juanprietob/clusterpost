f06ccddb5f4d4fe0d74f6cde7dae5a76fd942990
juanprietob <juanprietob@gmail.com>
2016-06-24 14:12:26 -0400

    ENH: Improve output from cronprovider

--- src/clusterpost-provider/cronprovider.js
+++ src/clusterpost-provider/cronprovider.js
@@ -7,16 +7,22 @@ module.exports = function (server, conf) {
 	var counter = 0;
 	var jobId = crontab.scheduleJob("*/30 * * * *", function(){
 
+		console.log("Running cronprovider service for running jobs...")
+
+		console.log("Retrieving runnnig jobs...");		
+
 		var view = "_design/searchjob/_view/jobstatus?key=" + JSON.stringify('RUN');
 	    server.methods.clusterprovider.getView(view)
 	    .then(function(docs){
 	    	var docs = _.pluck(docs, "value");
-	    	return Promise.map(docs, server.methods.executionserver.jobstatus);
-	    })
-	    .then(function(jobstatus){
-	    	_.each(jobstatus, function(rjs){
-	    		console.log(rjs);
-	    	});
+	    	return Promise.map(docs, server.methods.executionserver.jobstatus)
+	    	.then(function(jobstatus){
+		    	_.each(jobstatus, function(jst, i){
+		    		if(jst.status !== 'RUN'){
+		    			console.log("Job: ", docs[i]._id, ",", jst.status, "owner:", docs[i].userEmail);
+		    		}
+		    	});
+		    });
 	    })
 	    .catch(console.error);
 
