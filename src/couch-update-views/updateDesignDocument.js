def65689a08b1b4082bdb17c0ecd1b1fba979f40
juanprietob <juanprietob@gmail.com>
2016-11-10 17:49:27 -0500

    BUG: The design document is saved without duplicating the extension

--- src/couch-update-views/updateDesignDocument.js
+++ src/couch-update-views/updateDesignDocument.js
@@ -6,8 +6,8 @@ var path = require('path');
 
 module.exports = function(couchdb, viewsDir, design){
     
-    var uri = couchdb + "/_design/" + design;
-
+    var uri = couchdb + "/_design/" + design.replace('.json', '');
+    
     return new Promise(function(resolve, reject){
         request(uri, function(err, res, body){
             if(err) {                
@@ -16,7 +16,7 @@ module.exports = function(couchdb, viewsDir, design){
                 var jsonbody = JSON.parse(body);
                 delete jsonbody._rev;
                 var jsonstring = JSON.stringify(jsonbody, null, 4);                
-                var viewfilename = path.join(viewsDir, design + '.json');
+                var viewfilename = path.join(viewsDir, design);
                 try{
                     var write = fs.writeFileSync(viewfilename, jsonstring);
                     resolve({
