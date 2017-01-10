3007372d3d0695e0f1ae54767965fb3b33f5682f
juanprietob <juanprietob@gmail.com>
2017-01-10 12:31:42 -0500

    ENH: Add multiple namespaces

--- src/couch-provider/index.js
+++ src/couch-provider/index.js
@@ -10,58 +10,70 @@ exports.register = function (server, conf, next) {
     if(conf.namespace){
     	namespace = conf.namespace;
     }
+
+    var addNameSpace = function(namespace){
+    	server.method({
+		    name: namespace + '.getCouchDBServer',
+		    method: couchProvider.getCouchDBServer,
+		    options: {}
+		});
+
+		server.method({
+		    name: namespace + '.uploadDocuments',
+		    method: couchProvider.uploadDocuments,
+		    options: {}
+		});
+
+		server.method({
+		    name: namespace + '.getDocument',
+		    method: couchProvider.getDocument,
+		    options: {}
+		});
+
+		server.method({
+		    name: namespace + '.deleteDocument',
+		    method: couchProvider.deleteDocument,
+		    options: {}
+		});
+
+		server.method({
+		    name: namespace + '.addDocumentAttachment',
+		    method: couchProvider.addDocumentAttachment,
+		    options: {}
+		});
+
+		server.method({
+		    name: namespace + '.getDocumentURIAttachment',
+		    method: couchProvider.getDocumentURIAttachment,
+		    options: {}
+		});
+
+		server.method({
+		    name: namespace + '.getDocumentAttachment',
+		    method: couchProvider.getDocumentAttachment,
+		    options: {}
+		});
+
+		server.method({
+		    name: namespace + '.getView',
+		    method: couchProvider.getView,
+		    options: {}
+		});
+
+		console.info('couch-provider namespace', namespace, 'initialized.');
+    }
+
+
+    if(Array.isArray(namespace)){
+    	namespace.forEach(function(ns){
+    		addNameSpace(ns);
+    	});
+    }else{
+    	addNameSpace(namespace);
+    }
     
-	server.method({
-	    name: namespace + '.getCouchDBServer',
-	    method: couchProvider.getCouchDBServer,
-	    options: {}
-	});
 
-	server.method({
-	    name: namespace + '.uploadDocuments',
-	    method: couchProvider.uploadDocuments,
-	    options: {}
-	});
-
-	server.method({
-	    name: namespace + '.getDocument',
-	    method: couchProvider.getDocument,
-	    options: {}
-	});
-
-	server.method({
-	    name: namespace + '.deleteDocument',
-	    method: couchProvider.deleteDocument,
-	    options: {}
-	});
-
-	server.method({
-	    name: namespace + '.addDocumentAttachment',
-	    method: couchProvider.addDocumentAttachment,
-	    options: {}
-	});
-
-	server.method({
-	    name: namespace + '.getDocumentURIAttachment',
-	    method: couchProvider.getDocumentURIAttachment,
-	    options: {}
-	});
-
-	server.method({
-	    name: namespace + '.getDocumentAttachment',
-	    method: couchProvider.getDocumentAttachment,
-	    options: {}
-	});
-
-	server.method({
-	    name: namespace + '.getView',
-	    method: couchProvider.getView,
-	    options: {}
-	});
-
-	console.info('couch-provider namespace', namespace, 'initialized.');
-
-  return next();
+    return next();
 };
 
 exports.register.attributes = {
