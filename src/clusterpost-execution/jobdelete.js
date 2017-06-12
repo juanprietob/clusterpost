a3df753dac911d14c4765db198bc889b7bb6727d
Juan Carlos Prieto <juanprietob@gmail.com>
2017-06-12 10:17:06 -0400

    ENH: Check if the job is running.
    if the job is running kill the job

--- src/clusterpost-execution/jobdelete.js
+++ src/clusterpost-execution/jobdelete.js
@@ -2,24 +2,52 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function(jobid, conf){
+module.exports = function(jobid, conf, doc){
 
     var executionmethods = require('./executionserver.methods')(conf);
 
-    var cwd = path.join(conf.storagedir, jobid);
+    var promdoc; 
+
+    if(doc){
+        promdoc = Promise.resolve(doc);
+    }else{
+        promdoc = executionmethods.getDocument(jobid)
+        .catch(function(err){
+            console.error("Job not found before delete.", jobid)
+        });
+    }
+
+    return promdoc
+    .then(function(doc){        
+        return require(path.join(__dirname, "jobkill"))(doc, conf);
+    })
+    .then(function(){
+        var cwd = path.join(conf.storagedir, jobid);
     
-    executionmethods.deleteFolderRecursive(cwd);
-    var compressed = cwd + ".tar.gz";
-    var compressedstat;
-    try{
-        compressedstat = fs.statSync(compressed);
-    }catch(e){
-        //does not exist
-        compressedstat = undefined;
-    }
-    if(compressedstat){
-        fs.unlinkSync(compressed);
-    }
+        try{
+            executionmethods.deleteFolderRecursive(cwd);
+            var compressed = cwd + ".tar.gz";
+            var compressedstat;
+            try{
+                compressedstat = fs.statSync(compressed);
+            }catch(e){
+                //does not exist
+                compressedstat = undefined;
+            }
+            if(compressedstat){
+                fs.unlinkSync(compressed);
+            }
+        }catch(e){
+            return {
+                error: e
+            }
+        }
+        
+
+        return {
+            status: "Folder deleted " + jobid
+        }
+    });
         
     
 }
