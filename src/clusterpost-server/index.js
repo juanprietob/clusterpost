ecf490c66232667f27380b78f1f5aaa51295ce8b
juanprietob <juanprietob@gmail.com>
2018-04-23 17:46:55 -0400

    ENH: Update package dependencies versions clusterpost-server

--- src/clusterpost-server/index.js
+++ src/clusterpost-server/index.js
@@ -23,43 +23,19 @@ const startServer = function(cluster){
     var conf = getConfigFile();
     
     var server = new Hapi.Server();
-    var http_server;
 
-    if(conf.connections){
-        Object.keys(conf.connections).forEach(function(connection_label){
-            var connection = conf.connections[connection_label];
-            if(connection.tls){
-                const tls = {
-                    key: fs.readFileSync(connection.tls.key),
-                    cert: fs.readFileSync(connection.tls.cert)
-                }
-            }
-            
-            server.connection({ 
-                host: connection.host,
-                port: connection.port,
-                labels: [connection_label],
-                tls: tls
-            });
-        });
-
-        http_server = server.select("http");
-
-    }else{
-        var tls;
-        if(conf.tls && conf.tls.key && conf.tls.cert){
-            tls = {
-              key: fs.readFileSync(conf.tls.key),
-              cert: fs.readFileSync(conf.tls.cert)
-            };
-        }
-        server.connection({ 
-            host: conf.host,
-            port: conf.port,
-            tls: tls
-        });
-        http_server = server;
+    var tls;
+    if(conf.tls && conf.tls.key && conf.tls.cert){
+        tls = {
+          key: fs.readFileSync(conf.tls.key),
+          cert: fs.readFileSync(conf.tls.cert)
+        };
     }
+    server.connection({ 
+        host: conf.host,
+        port: conf.port,
+        tls: tls
+    });    
 
     var plugins = [];
 
@@ -86,7 +62,7 @@ const startServer = function(cluster){
     });
 
     
-    http_server.method({
+    server.method({
         name: 'getCluster',
         method: function(){
             return cluster;
@@ -95,7 +71,7 @@ const startServer = function(cluster){
     });
     
 
-    http_server.register(plugins, function(err){
+    server.register(plugins, function(err){
         if (err) {
             throw err; // something bad happened loading the plugin
         }
@@ -104,7 +80,7 @@ const startServer = function(cluster){
     
     server.start(function () {
         server.connections.forEach(function(connection){
-            server.log('info', 'server is listening port: ' + connection.info.uri + " label: " + connection.settings.labels);
+            server.log('info', 'server is listening port: ' + connection.info.uri);
         });
     });
 
