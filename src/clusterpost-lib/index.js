7f814206ebd0ca1a829ac5c088f13fec9b3bcd04
Juan Carlos Prieto <juanprietob@gmail.com>
2018-10-16 17:37:29 -0400

    ENH: add functions to start the clusterpostlib

--- src/clusterpost-lib/index.js
+++ src/clusterpost-lib/index.js
@@ -15,7 +15,7 @@ var HapiJWTCouch = require('hapi-jwt-couch-lib')
 class ClusterpostLib extends HapiJWTCouch{
     constructor(){
         super()
-        this.configfilename = '.clusterpost-config.json';
+        this.configfilename = path.join(process.cwd(), '.clusterpost-config.json');
         this.joiconf = Joi.object().keys({
             uri: Joi.string().uri(),
             token: Joi.string()
@@ -27,14 +27,26 @@ class ClusterpostLib extends HapiJWTCouch{
         });
     }
 
+    setConfigFileName(configfilename){
+        this.configfilename = configfilename;
+    }
+
+    getConfigFileName(){
+        return this.configfilename;
+    }
+
     setClusterPostServer (uri){
         this.setServer(uri);
+    }
+
+    getClusterPostServer(){
+        return this.getServer()
     }
 
     getConfigFile() {
       try {
         // Try to load the user's personal configuration file in the current directory
-        var conf = require(path.join(process.cwd(), this.configfilename));
+        var conf = require(this.configfilename);
         Joi.assert(conf, this.joiconf);
         return conf;
       } catch (e) {
@@ -45,7 +57,7 @@ class ClusterpostLib extends HapiJWTCouch{
     setConfigFile (config) {
       try {
         // Try to save the user's personal configuration file in the current working directory
-        var confname = path.join(process.cwd(), this.configfilename);
+        var confname = this.configfilename;
         console.log("Writing configuration file", confname);
         fs.writeFileSync(confname, JSON.stringify(config));
       } catch (e) {
@@ -486,7 +498,7 @@ class ClusterpostLib extends HapiJWTCouch{
         var prom;
         var self = this;
         if(files){
-            prom = checkFiles(files)
+            prom = self.checkFiles(files)
             .then(function(){
                 return self.createDocument(job);
             })
@@ -578,6 +590,107 @@ class ClusterpostLib extends HapiJWTCouch{
                 }
             });
         });
+    }
+
+    promptUsernamePassword(){
+        return new Promise(function(resolve, reject){
+            var schema = {
+                properties: {
+                    email: {
+                        pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        message: 'Email address',
+                        required: true
+                    },
+                    password: {                    
+                        hidden: true,
+                        required: true
+                    }
+                }
+            };
+            prompt.start();
+            prompt.get(schema, function (err, result) {
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    promptClusterpostServer(){
+        return new Promise(function(resolve, reject){
+            var schema = {
+                properties: {
+                    uri: {
+                        message: 'Please enter the clusterpost server uri',
+                        required: true
+                    }
+                }
+            };
+            prompt.start();
+            prompt.get(schema, function (err, result) {
+                if(err){
+                    reject(err)
+                }else{
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+    testUserToken(token){
+        var jwt = jws.decode(token.token);
+
+        if(jwt.exp && jwt.exp < Date.now() / 1000){
+            return false;
+        }else if(jwt.exp === undefined){
+            console.log("WARNING! The token does not have an expiry date. Tokens without expiry date were deprecated. The clusterpost-server could be running an old version. Please contact the clusterpost-server administrator.");
+        }
+        return true;
+    }
+
+    start(configfilename){
+        var self = this;
+
+        if(configfilename){
+            self.setConfigFileName(configfilename);
+        }
+        var config = self.getConfigFile();
+
+        if(config){
+            self.setClusterPostServer(config.uri);
+            if(self.testUserToken(config)){
+                self.setUserToken(config);
+                return Promise.resolve();
+            }else{
+                return self.promptUsernamePassword()
+                .then(function(user){
+                    return self.userLogin(user);
+                })
+                .then(function(token){
+                    _.extend(token, {
+                        uri: self.getClusterPostServer()
+                    });
+                    self.setConfigFile(token);
+                });
+            }
+        }else{
+            return self.promptClusterpostServer()
+            .then(function(server){
+                self.setClusterPostServer(server);
+                return self.promptUsernamePassword()
+            })
+            .then(function(user){
+                return self.userLogin(user);
+            })
+            .then(function(token){
+                _.extend(token, {
+                    uri: self.getClusterPostServer()
+                });
+                self.setConfigFile(token);
+            });
+        }
     }
 
 }
