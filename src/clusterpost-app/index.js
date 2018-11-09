0117f09004040a4d6b8d82f03bf63a5e3b0284b5
Juan Carlos Prieto <juanprietob@gmail.com>
2018-11-09 14:12:07 -0500

    ENH: Update how the app starts clusterpost

--- src/clusterpost-app/index.js
+++ src/clusterpost-app/index.js
@@ -13,99 +13,22 @@ var agentoptions = {
 
 clusterpost.setAgentOptions(agentoptions);
 
-
-const getConfigFile = function () {
-    return new Promise(function(resolve, reject){
-        try {
-            // Try to load the user's personal configuration file
-            var conf = path.join(os.homedir(), '.clusterpost.json');
-            resolve(require(conf));
-        } catch (e) {            
-            reject(e);
-        }
-    });
-};
-
 const help = function(){
     console.error("Help: Download tasks from the server.");
-    console.error("Required parameters when login for first time:");
-    console.error("--server url, set the server url. ex., https://some.server:8180");    
     console.error("\nOptional parameters:");
     console.error("--dir  Output directory, default: ./out");
-    console.error("--status one of [DONE, RUN, FAIL, EXIT, UPLOADING, CREATE], default: None");
+    console.error("--status one of [DONE, RUN, FAIL, EXIT, UPLOADING, CREATE], default: DONE");
     console.error("--print , if provided the information is printed only");
     console.error("--delete, default false, when downloading jobs with status 'DONE', the jobs will be deleted upon completion");
-    console.error("--j job id, default: all ids");
+    console.error("--j job id, default: ");
     console.error("--executable executable, default: all executables");
     console.error("--email email, default: (authenticated user)");
+    console.error("--config_codename codename, default: clusterpost");
 }
 
 if(argv["h"] || argv["help"]){
     help();
     process.exit(1);
-}
-
-var writeConfFile = function(conf){
-    var confpath = path.join(os.homedir(), '.clusterpost.json');
-    console.log("Writting configuration file with server information and token to:", confpath);
-    console.log("You won't need to type the server information next time.");
-    console.log("If you have authentication problems in the future, please delete this file and type the login information again.");
-
-    fs.writeFileSync(confpath, JSON.stringify(conf));
-}
-
-var login = function(user){
-    return clusterpost.userLogin(user);
-}
-
-var getUsernamePassword = function(){
-    return new Promise(function(resolve, reject){
-        var schema = {
-            properties: {
-                email: {
-                    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-                    message: 'Email address',
-                    required: true
-                },
-                password: {                    
-                    hidden: true,
-                    required: true
-                }
-            }
-        };
-        prompt.start();
-        prompt.get(schema, function (err, result) {
-            resolve(result);
-        });
-    });
-}
-
-var loginprom = undefined;
-if(argv["server"]){
-
-    var conf = {};
-    conf.server = argv["server"];
-
-    clusterpost.setClusterPostServer(conf.server);
-
-    loginprom = 
-    getUsernamePassword()
-    .then(function(user){
-        return login(user);
-    })
-    .then(function(res){
-        conf.token = res.token;
-        writeConfFile(conf);
-    });
-}else{
-    loginprom = getConfigFile()
-    .then(function(conf){
-        clusterpost.setClusterPostServer(conf.server);
-        clusterpost.setUserToken(conf.token);
-    })
-    .catch(function(e){
-        throw "Config file not found. Use -h or --help to learn how to use this program";
-    });
 }
 
 var deletejobs = false;
@@ -124,7 +47,10 @@ if(argv["dir"]){
     outputdir = argv["dir"];
 }
 
-var status = argv["status"];
+var status = 'DONE';
+if(argv["status"]){
+    status = argv["status"];
+}
 var jobid = argv["j"];
 var executable = argv["executable"];
 var print = argv["print"];
@@ -143,8 +69,12 @@ if(print){
     console.log("print", print);   
 }
 
+var config_codename = 'clusterpost';
+if(argv["config_codename"]){
+    config_codename = argv["config_codename"];
+}
 
-loginprom
+clusterpost.start(path.join(os.homedir(), '.' + config_codename + '.json'))
 .then(function(){
 
     if(!print){
