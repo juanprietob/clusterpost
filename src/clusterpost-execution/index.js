01d35607334ed09026e80b7104d7be289ff6d6cc
Juan Carlos Prieto <juanprietob@gmail.com>
2021-03-30 11:31:38 -0500

    ENH: Add global install

--- src/clusterpost-execution/index.js
+++ src/clusterpost-execution/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 
 var _ = require('underscore');
 var argv = require('minimist')(process.argv.slice(2));
--- src/clusterpost-execution/postinstall.js
+++ src/clusterpost-execution/postinstall.js
@@ -2,27 +2,18 @@ var fs = require('fs');
 var path = require('path');
 
 var cwd = process.cwd();
-var installdir = path.join(process.cwd(), "../../");
-
-var index = fs.readFileSync(path.join(cwd, "index.js.in"));
+var defaultconfig = path.join(__dirname, "conf.json.in");
+var userconf = path.join(cwd, 'conf.json');
 
 try{
-	var stats = fs.statSync(path.join(installdir, 'index.js'));
+	console.log(userconf)
+	var stats = fs.statSync(userconf);
 }catch(e){
-	console.log("Generating default index file at:", installdir, "index.js");
+	console.log("Generating default configuration file at", userconf);
 	console.log("Please edit this file with your configuration parameters.");
-	fs.writeFileSync(path.join(installdir, 'index.js'), index);
-
-	var defaultconfig = fs.readFileSync(path.join(cwd, "conf.json.in"));
-
-	try{
-		var stats = fs.statSync(path.join(installdir, 'conf.json'));
-	}catch(e){
-		console.log("Generating default configuration file at", installdir, "conf.json");
-		console.log("Please edit this file with your configuration parameters.");
-		fs.writeFileSync(path.join(installdir, 'conf.json'), defaultconfig);
-	}
+	fs.writeFileSync(userconf, fs.readFileSync(defaultconfig));
 }
+
 
 
 
