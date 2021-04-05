eb9a362990b1ce4c37238e1ba3d652893da161cd
Juan Carlos Prieto <juanprietob@gmail.com>
2021-04-05 09:29:30 -0500

    ENH: Change default config location

--- src/clusterpost-execution/postinstall.js
+++ src/clusterpost-execution/postinstall.js
@@ -1,19 +1,17 @@
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 
 var cwd = process.cwd();
 var defaultconfig = path.join(__dirname, "conf.json.in");
 var userconf = path.join(cwd, 'conf.json');
 
 try{
-	console.log(userconf)
 	var stats = fs.statSync(userconf);
+	console.log(chalk.green("Your configuration file is at"), chalk.green(userconf));
 }catch(e){
-	console.log("Generating default configuration file at", userconf);
-	console.log("Please edit this file with your configuration parameters.");
+	console.log(chalk.green("Generating default configuration file at"), chalk.green(userconf));
+	console.log(chalk.green("Please edit this file with your configuration parameters."));
+	console.log(chalk.green("The token.json should be saved into this directory as well."));
 	fs.writeFileSync(userconf, fs.readFileSync(defaultconfig));
 }
-
-
-
-
