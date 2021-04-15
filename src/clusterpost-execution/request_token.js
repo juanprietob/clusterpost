2150a8c656795ab6fcf3bb28f4931650a6386fd4
Juan Carlos Prieto <juanprietob@gmail.com>
2021-04-15 10:28:22 -0500

    ENH: Request token via script

--- src/clusterpost-execution/request_token.js
+++ src/clusterpost-execution/request_token.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const clusterpost = require("clusterpost-lib");
-const clustermodel = require("clustterpost-model");
+const clustermodel = require("clusterpost-model");
 const Joi = require('joi');
 const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+const fs = require('fs');
 
 var agentoptions = {
     rejectUnauthorized: false
@@ -11,13 +13,28 @@ var agentoptions = {
 
 clusterpost.setAgentOptions(agentoptions);
 
+var help = function(){
+    console.log(chalk.cyan("help: Request token from server"));
+    console.log(chalk.green("Options:"));
+    console.log(chalk.green("--es               <execution server name>"));
+}
+
+
+var executionserver = argv["es"];
+
+if(!executionserver || argv["h"] || argv["help"]){
+    help();
+    process.exit();
+}
+
 clusterpost.start()
 .then(function(){
-	return clusterpost.getExecutionServerToken(executionserver);
+	return clusterpost.getExecutionServerToken({executionserver})
 	.then(function(token){
+		token = token[0]
 		Joi.assert(token, clustermodel.executionservertoken);
-		console.log("Writing token to conf.json");
-		fs.writeFileSync('conf.json', JSON.stringify(token));
-	}).
+		console.log("Writing token to token.json");
+		fs.writeFileSync('token.json', JSON.stringify(token));
+	})
 })
 .catch(console.error)
