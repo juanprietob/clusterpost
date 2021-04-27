3a3260da2599bdf32a3d106f63faea044eed80c3
Juan Carlos Prieto <juanprietob@gmail.com>
2021-04-27 10:19:59 -0400

    BUG: Wrap create dir in try catch
    It's ok if it doesn't create the directory when in a container. The config will be set manually

--- src/clusterpost-execution/postinstall.js
+++ src/clusterpost-execution/postinstall.js
@@ -9,16 +9,20 @@ var defaultconfig = path.join(__dirname, "conf.json.in");
 var user_conf_dir = path.join(os.homedir(), '.clusterpost-execution');
 var user_conf = path.join(user_conf_dir, 'conf.json');
 
-if(!fs.existsSync(user_conf_dir)){
-	fs.mkdirSync(user_conf_dir, {recursive: true});
-}
-
 try{
-	var stats = fs.statSync(user_conf);
-	console.log(chalk.green("Your configuration file is at"), chalk.green(user_conf));
+	if(!fs.existsSync(user_conf_dir)){
+		fs.mkdirSync(user_conf_dir, {recursive: true});
+	}
+
+	try{
+		var stats = fs.statSync(user_conf);
+		console.log(chalk.green("Your configuration file is at"), chalk.green(user_conf));
+	}catch(e){
+		console.log(chalk.green("Generating default configuration file at"), chalk.green(user_conf));
+		console.log(chalk.green("Please edit this file with your configuration parameters."));
+		console.log(chalk.green("The token.json should be saved into this directory as well."));
+		fs.writeFileSync(user_conf, fs.readFileSync(defaultconfig));
+	}
 }catch(e){
-	console.log(chalk.green("Generating default configuration file at"), chalk.green(user_conf));
-	console.log(chalk.green("Please edit this file with your configuration parameters."));
-	console.log(chalk.green("The token.json should be saved into this directory as well."));
-	fs.writeFileSync(user_conf, fs.readFileSync(defaultconfig));
+	console.error(chalk.red("Installation was not able to write config file at"), chalk.green(user_conf))
 }
